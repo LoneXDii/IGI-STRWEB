dd695d029b0c08b37f2a176f649f32ca9b83d5a4
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-03-06 21:06:49 +0300

    Adde tasks 1-5 LR2

--- IGI/LR2/Django-React_SQLite/backend/src/deliveries/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Deliveries, Truck, Users, WebsiteContacts
-
-# Register your models here.
-admin.site.register(Deliveries)
-admin.site.register(Truck)
-admin.site.register(Users)
-admin.site.register(WebsiteContacts)
