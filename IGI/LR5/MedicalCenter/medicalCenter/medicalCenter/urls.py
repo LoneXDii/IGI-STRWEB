b83a06e0d44a809abbea0b7622d2b5fe4dd2e92d
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-04-22 21:45:46 +0300

    Added base pages and redirects

--- IGI/LR5/MedicalCenter/medicalCenter/medicalCenter/urls.py
+++ IGI/LR5/MedicalCenter/medicalCenter/medicalCenter/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import django
 from django.contrib import admin
-from django.urls import path
+import django.contrib.auth.urls
+from django.urls import include, path
+from medicalCenter_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('about/', views.about, name='about'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('coupons/', views.coupons, name='coupons'),
+    path('news/', views.news, name='news'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('reviews/', views.reviews, name='reviews'),
+    path('terms/', views.terms_and_defs, name='terms'),
+    path('vacancies/', views.vacancies, name='vacancies'),
 ]
--- IGI/LR5/MedicalCenter/medicalCenter/medicalCenter_app/views.py
+++ IGI/LR5/MedicalCenter/medicalCenter/medicalCenter_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def about(request):
+    return render(request, 'about.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+def coupons(request):
+    return render(request, 'coupons.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def news(request):
+    return render(request, 'news.html')
+
+def privacy(request):
+    return render(request, 'privacy.html')
+
+def reviews(request):
+    return render(request, 'reviews.html')
+
+def terms_and_defs(request):
+    return render(request, 'termsAndDefs.html')
+
+def vacancies(request):
+    return render(request, 'vacancies.html')
